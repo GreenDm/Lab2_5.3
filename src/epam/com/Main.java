5bfe8edeba32ba43122f02fa22952a62a6f70b97
Green <dr@mail.ru>
2016-02-22 20:52:52 +0200

    Initial commit

---
+++ src/epam/com/Main.java
@@ -0,0 +1,16 @@
+package epam.com;
+
+/**
+ * Created by m18 on 22.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        MyInitTest m1=new MyInitTest();
+        MyInitTest m2=new MyInitTest();
+
+
+        m1.printArray();
+        m2.printArray();
+    }
+}
